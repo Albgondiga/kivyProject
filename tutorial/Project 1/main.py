94fba9295b462c9beb51c41a0c37d03accd10982
Alberto González Avendaño <albertoga03@gmail.com>
2023-12-22 00:15:06 -0600

    Complete Kivy Lang code

--- tutorial/Project 1/main.py
+++ tutorial/Project 1/main.py
@@ -6,22 +6,26 @@ from kivy.uix.textinput import TextInput
 
 
 class Interface(FloatLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        btn=Button(text="Click Me!", size_hint=(0.5,0.1), pos_hint={"center_x":0.5, "center_y":0.5})
-        self.label=Label(text="Nothing!", size_hint=(0.5,0.1), pos_hint={"center_x":0.5, "center_y":0.6})
-        self.textInput=TextInput( size_hint=(0.5,0.1), pos_hint={"center_x":0.5, "center_y":0.7}, multiline=False)
-        self.textInput.bind(on_text_validate=self.display_information)
-        btn.bind(on_press=self.display_information)
-        self.add_widget(btn)
-        self.add_widget(self.label)
-        self.add_widget(self.textInput)
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     btn=Button(text="Click Me!", size_hint=(0.5,0.1), pos_hint={"center_x":0.5, "center_y":0.5})
+    #     self.label=Label(text="Nothing!", size_hint=(0.5,0.1), pos_hint={"center_x":0.5, "center_y":0.6})
+    #     self.textInput=TextInput( size_hint=(0.5,0.1), pos_hint={"center_x":0.5, "center_y":0.7}, multiline=False)
+    #     self.textInput.bind(on_text_validate=self.display_information)
+    #     btn.bind(on_press=self.display_information)
+    #     self.add_widget(btn)
+    #     self.add_widget(self.label)
+    #     self.add_widget(self.textInput)
 
-    def display_information(self, obj):
-        data=self.textInput.text
-        self.label.text = data
+    #def display_information(self, obj):
+    def display_information(self):
+        #data=self.textInput.text
+        data = self.ids.textInput.text
+        #self.label.text = data
+        self.ids.label.text=data
 class ProjectApp(App):
-    def build(self):
-        return Interface()
+    # def build(self):
+    #     return Interface()
+    pass
 
 ProjectApp().run()
