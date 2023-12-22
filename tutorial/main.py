8ec24adc4d613b9922842476000ccac2dc83a58f
Alberto González Avendaño <albertoga03@gmail.com>
2023-12-21 23:39:28 -0600

    Creating elements using Python.

--- tutorial/main.py
+++ tutorial/main.py
@@ -1,15 +1,20 @@
 from kivy.app import App
+from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 
 
 class Interface(FloatLayout):
-    def focus(self):
-        print("Focused")
-    def printing_msg(self):
-        print("Hello World!")
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        btn = Button(text="Click Me!", size_hint=(0.5,0.5), pos_hint={"center_x":0.5, "center_y":0.5})
+        btn.bind(on_press=self.button_press)
+        self.add_widget(btn)
+
+    def button_press(self, obj):
+        print(obj.text)
+        #print(obj.size)
 
 class FirstApp(App):
-    def printing_msg(self):
-        print("Hello World!")
+    pass
 
 FirstApp().run()
